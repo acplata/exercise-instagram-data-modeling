929e01e079b34c2ed444eb39c2ceb9bc3520e262
acplata <133425402+acplata@users.noreply.github.com>
2023-08-15 17:37:28 +0000

    ig

--- src/models.py
+++ src/models.py
@@ -7,31 +7,51 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+    name = Column(String, nullable=False)
+    lastname = Column(String, nullable=False)
+    username = Column(String(20), nullable=False)
+    email = Column(String(60), nullable=False)
+    password = Column(String(20), nullable=False)
+
+    posts = relationship("Post", backref="user")
+    likes = relationship("Like", backref="user")
+    comments = relationship("Comment", backref="user")
+
+class Post(Base):
+    __tablename__ = "post"
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    title = Column(String(10), nullable=False)
+    caption = Column(String(100), nullable=False)
+    created_by_user_id = Column(String(20), nullable=False)
+    created_by_user_date = Column(String, nullable=False)
 
-    def to_dict(self):
-        return {}
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
+    
+    likes = relationship("Like", backref="post")
+    comments = relationship("Comment", backref="post")
 
-## Draw from SQLAlchemy base
-try:
-    result = render_er(Base, 'diagram.png')
-    print("Success! Check the diagram.png file")
-except Exception as e:
-    print("There was a problem genering the diagram")
-    raise e
+class Like(Base):
+    __tablename__ = "like"
+    id = Column(Integer, primary_key=True)
+
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
+
+    post_id = Column(Integer, ForeignKey("post.id"), nullable=True)
+    comment_id = Column(Integer, ForeignKey("comment.id"), nullable=True)
+
+class Comment(Base):
+    __tablename__ = "comment"
+    id = Column(Integer, primary_key=True)
+    content = Column(String(100), nullable=False)
+
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
+    post_id = Column(Integer, ForeignKey("post.id"), nullable=False)
+
+    likes = relationship("Like", backref="comment")
+
+render_er(Base, 'diagram.png')
